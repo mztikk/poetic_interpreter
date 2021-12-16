949e23841763db57ffb16fddc322ea2262dc3664
Nils Feierabend <mztikk@outlook.de>
2021-12-16 21:34:31 +0100

    use new poetic structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use atty::Stream;
-use poetic::{Interpreter, Parser};
+use poetic::{interpreter::Interpreter, parser::Parser};
 use std::io::{self, Read};
 
 fn main() {
@@ -11,9 +11,8 @@ fn main() {
     let mut buf = String::new();
     stdin.read_to_string(&mut buf).unwrap();
 
-    let mut interpreter = Interpreter::new();
-    let mut parser = Parser::new(buf);
-    for instruction in parser.code {
-        interpreter.execute(&instruction);
-    }
+    let intermediate = Parser::parse_intermediate(&buf);
+    let code = Parser::parse_instructions(&intermediate);
+    let mut interpreter = Interpreter::new(code);
+    interpreter.run();
 }
