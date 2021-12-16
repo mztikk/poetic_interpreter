ebf6fea70fc9b860b3db61bf4627db7f27f11cb0
Nils Feierabend <mztikk@outlook.de>
2021-12-16 21:53:57 +0100

    time measure prints

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use atty::Stream;
 use poetic::{interpreter::Interpreter, parser::Parser};
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    time::Instant,
+};
 
 fn main() {
     if atty::is(Stream::Stdin) {
@@ -11,8 +14,24 @@ fn main() {
     let mut buf = String::new();
     stdin.read_to_string(&mut buf).unwrap();
 
+    let run_now = Instant::now();
+
+    let intermediate_now = Instant::now();
     let intermediate = Parser::parse_intermediate(&buf);
+    println!(
+        "parsing to intermediate took {}",
+        intermediate_now.elapsed().as_secs_f64()
+    );
+
+    let instructions_now = Instant::now();
     let code = Parser::parse_instructions(&intermediate);
+    println!(
+        "parsing to instructions took {}",
+        instructions_now.elapsed().as_secs_f64()
+    );
+
     let mut interpreter = Interpreter::new(code);
     interpreter.run();
+
+    println!("run took {}", run_now.elapsed().as_secs_f64());
 }
